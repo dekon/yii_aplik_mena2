2ab2446021ca2accdf905767fdec41742a18c006
Dekon <dekon0@gmail.com>
2012-07-24 20:36:09 +0200

    pozycja frama

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'yii/framework1.1.7/yii.php';
+$yii=dirname(__FILE__).'/yii/framework1.1.7/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
